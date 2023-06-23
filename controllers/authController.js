aa7889358d4b667a4538c1486c5ab4410e22d691
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-23 20:05:38 +0530

    content updated with authHelper using bcrypt  ..

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,36 @@
-const registerController = () =>{
+import userModel from "../models/userModel.js";
 
+
+export const registerController = async (req ,res) =>{
+    try {
+
+        const {name,email,password,phone,address} = req.body
+
+        //validation Properties
+        if (!name) {
+           return res.send({error:'Name is Required !!!',});
+        }
+        if (!email) {
+           return res.send({error:'Email is Required !!!',});
+        }
+        if (!password) {
+           return res.send({error:'Password is Required !!!',});
+        }
+        if (!phone) {
+           return res.send({error:'Phone is Required !!!',});
+        }
+        if (!address) {
+           return res.send({error:'Address is Required !!!',});
+        }
+
+
+
+    }catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:'Error in Registration ...',
+            error
+        })
+    }
 }
 
-export default {registerController}
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan'
 import connectDB from "./config/db.js";
-
+import authRoutes from "./routes/authRoutes.js";
 
 //configure env
 dotenv.config();
@@ -18,6 +18,10 @@ const app = express()
 //middlewares
 app.use(express.json())
 app.use(morgan('dev'))
+
+
+//routes
+app.use(`/api/v1/auth`,authRoutes);
 
 //rest api
 app.get('/', (req, res) => {
