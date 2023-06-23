6900250c0c1894f52aeec167b1ae0b06f385e465
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-23 11:19:16 +0530

    Sequre server using dotenv proceed ...

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const colors = require('colors')
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+
+//configure env
+dotenv.config();
 
 //rest object
 const app = express()
 
 //rest api
-app.get('/',(req, res) => {
-
-res.send({
-    message:"welcome to ecommerce app"
-})
+app.get('/', (req, res) => {
+    res.send("<h1>welcome to ecommerce app with mern stack</h1>")
 })
 
-//port
-const PORT = 8080
+
+//port using env
+const PORT = process.env.PORT || 8080;
 
 //run listen
-app.listen(PORT,()=>{
-    console.log(`Server Running on ${PORT}`.bgCyan.white);
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 })
