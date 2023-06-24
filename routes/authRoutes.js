65d48bfbfa1262abcba87cdbff44d6e294c634f0
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-24 13:32:13 +0530

    Secured Login proceed using JWT Token ...

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerController} from "../controllers/authController.js";
+import { registerController , loginController } from "../controllers/authController.js";
 
 //router object
 const router = express.Router();
@@ -7,8 +7,11 @@ const router = express.Router();
 
 //routing
 
-/**  Register || Method POST  */
+/**  REGISTER || Method POST  */
 router.post("/register",registerController)
 
+
+/** LOGIN  || POST  */
+router.post("/login",loginController)
 
 export default router;
