b56ff94f44b2bb6bdf7334fb3836ceff673eca7b
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-06-24 14:18:48 +0530

    create class authMiddleware.js for implement authorization in admin role...

--- controllers/authController.js
+++ controllers/authController.js
@@ -121,3 +121,8 @@ export const loginController = async (req ,res) => {
     }
 };
 
+//test controller
+export const testController = ( req , res )=>{
+    res.send("protected route ...")
+}
+
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { registerController , loginController } from "../controllers/authController.js";
-
+import { registerController , loginController , testController } from "../controllers/authController.js";
+import {isAdmin, requireSignIn} from "../middlewares/authMiddleware.js";
 //router object
 const router = express.Router();
 
@@ -13,5 +13,8 @@ router.post("/register",registerController)
 
 /** LOGIN  || POST  */
 router.post("/login",loginController)
+
+//test routes
+router.get('/test',requireSignIn , isAdmin , testController)
 
 export default router;
